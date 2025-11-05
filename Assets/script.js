d8b15b902d75b796c2f988a384cd4016367a6273
Rey-Berkelmans <143152147+Rey-Berkelmans@users.noreply.github.com>
2025-11-05 01:14:18 +0000

    Content - Login Page

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,14 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+ function fun() {
+        let inputPswd = document.getElementById("validate")
+                                .value;
+        let correctPswd = "2005Mcmillan";
+        if (inputPswd === correctPswd) {
+            window.open("noahcameron.html");
+        } else {
+            alert("Incorrect password!");
+        }
+    }
